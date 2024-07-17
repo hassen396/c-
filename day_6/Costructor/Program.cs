246cfdcc01c742aceb7b0853cecea82f1fcbc1ea
hassen muhammed <120062084+hassen396@users.noreply.github.com>
2024-07-17 09:31:09 +0300

    day 6

--- day_6/Costructor/Program.cs
+++ day_6/Costructor/Program.cs
@@ -1,13 +1,35 @@
-﻿using system;
-class Bird {
-    public Bird(string color){
+﻿
+using System;
+class Bird
+{
+    public string name;
+    public string color;
+    public Bird(string color, string name)
+    {
         Console.WriteLine("Bird constructor is called");
-        string color = color;
-        Console.WriteLine($"The bird is {color}");
+        this.name = name;
+        this.color = color;
+        Console.WriteLine($"{name} is {color}");
     }
+    public bool CanFly;
 }
-class Program {
-    static void Main(string[] args) {
-        Bird b = new Bird();
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        Bird parrot = new Bird("green", "parrot");
+        parrot.CanFly = true;
+        if (parrot.CanFly)
+        {
+            Console.WriteLine($"{parrot.name} can fly");
+        }
+        else
+        {
+            Console.WriteLine($"{parrot.name} cannot fly");
+        }
+        Console.WriteLine($"tha name of the bird is {parrot.name} and the color  is {parrot.color}");
+
+
     }
 }
--- day_6/Costructor/obj/Debug/net8.0/Costructor.AssemblyInfo.cs
+++ day_6/Costructor/obj/Debug/net8.0/Costructor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Costructor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46c4511f383cad92adb656168038f386bdd1d945")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cbbc02dfbc8dfc03192142b6705b761f38a01fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Costructor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Costructor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
