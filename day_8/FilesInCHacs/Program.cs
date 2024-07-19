44fc5cb96c7e487a557755881e453fce5d7b8a05
hassen muhammed <120062084+hassen396@users.noreply.github.com>
2024-07-19 11:20:54 +0300

    day 8

---
+++ day_8/FilesInCHacs/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+class Program{
+    static void Main(string[] args) {
+        // Console.Write("enter a massge to save into file: ");
+        // string wrtireFile = Console.ReadLine();
+        // File.WriteAllText("firstfile.txt", wrtireFile);
+        string readFile = File.ReadAllText("firstfile.txt");
+        Console.WriteLine(readFile);// writing a massage to my readers from a file
+
+    }
+}
